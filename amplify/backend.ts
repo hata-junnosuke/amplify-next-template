5b641b99746e77a33eb2328b56ccd8bcc1ac3c0e
hata-junnosuke <jh19911209@icloud.com>
2024-05-14 14:34:09 +0900

    backend更新

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource.js';
-import { data } from './data/resource.js';
+import { data } from './data/resource';
 
-defineBackend({
-  auth,
-  data,
+const backend = defineBackend({
+  data
 });
+
+const dataResources = backend.data.resources;
+
+dataResources.cfnResources.cfnGraphqlApi.xrayEnabled = true;

6735f03938bd7093d55644e5e68cd9c364e2c0ed
hata-junnosuke <jh19911209@icloud.com>
2024-05-22 12:11:41 +0900

    test

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -9,14 +9,14 @@ export const auth = defineAuth({
     externalProviders: {
       google: {
         // @ts-ignore
-        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
         // @ts-ignore
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
       },
       callbackUrls: [
-        'http://localhost:3001',
+        'http://localhost:3001'
       ],
-      logoutUrls: ['http://localhost:3001/'],
+      logoutUrls: ['http://localhost:3001'],
     }
   },
 });

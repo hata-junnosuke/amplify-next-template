c149f64e2ef51ae33448852b9ca5dfb30c5dfd96
hata-junnosuke <jh19911209@icloud.com>
2024-05-23 14:54:26 +0900

    ストレージ追加

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,12 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 // import { data } from './data/resource.js';
+import { storage } from './storage/resource';
 
 const backend = defineBackend({
   auth,
   // data,
+  storage,
 });
 
 backend.addOutput({
